abdae7638f82f24c839935fb13bfbfe31407af47
LindaHG1 <116809226+LindaHG1@users.noreply.github.com>
2023-02-07 13:15:59 +0100

    Faltan solo los colores

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
+import './components/container/Tiempo.css'
 import Formulario from './components/container/Formulario';
+import WeatherPanel from './components/container/WeatherPanel';
 {/* import NOMBREdelCOMPONENTE from './components/UBICACIONdelCOMPONENTE'; */}
 
 function App() {
@@ -8,6 +10,7 @@ function App() {
       <h1>Hola chicas</h1>
       {/* aquí declaramos nuestro COMPONENTE propio */}
       {/* <Componente></Componente> */}
+      <WeatherPanel />
       <Formulario></Formulario>
     </div>
   );
---
+++ src/components/container/Cards.jsx
@@ -0,0 +1,114 @@
+import React from "react";
+import Spinner from "./Spinners";
+
+const Card = ({loadingData, showData, weather, forecast}) => {
+
+    let today = new Date();
+    let day = today.getDate();
+    let month = today.getMonth();
+    let year = today.getFullYear();
+    let date = day + '/' + month + '/' + year;
+
+    let url = '';
+    let iconUrl = '';
+
+    let iconUrl3 = '';
+    let iconUrl6 = '';
+    let iconUrl9 = '';
+
+    let forecastDate3 = '';
+    let forecastDate6 = '';
+    let forecastDate9 = '';
+
+
+
+    if(loadingData){
+        return <Spinner />;
+    }
+
+    if(showData){
+        url = 'http://openweathermap.org/img/w/';
+        iconUrl = url + weather.weather[0].icon + '.png'
+
+        iconUrl3 = url + forecast.list[1].weather[0].icon + '.png';
+        iconUrl6 = url + forecast.list[2].weather[0].icon + '.png';
+        iconUrl9 = url + forecast.list[3].weather[0].icon + '.png';
+
+        forecastDate3 = forecast.list[1].dt_txt.substring(8, 10) + '/' + forecast.list[1].dt_txt.substring(5, 7) + '/' + forecast.list[1].dt_txt.substring(0, 4) + ' ' +  forecast.list[1].dt_txt.substring(11,13);
+
+        forecastDate6 = forecast.list[2].dt_txt.substring(8, 10) + '/' + forecast.list[2].dt_txt.substring(5, 7) + '/' + forecast.list[2].dt_txt.substring(0, 4) + ' ' +  forecast.list[2].dt_txt.substring(11,13);
+
+        forecastDate9 = forecast.list[3].dt_txt.substring(8, 10) + '/' + forecast.list[3].dt_txt.substring(5, 7) + '/' + forecast.list[3].dt_txt.substring(0, 4) + ' ' +  forecast.list[3].dt_txt.substring(11,13);
+
+
+
+    }
+
+    return(
+        
+        <div className="mt-5">
+            {
+                showData === true ? (
+
+                    <div className="container">
+                        <div className="card mb-3 mx-auto bg-dark text-light">
+                            <div className="row g-0">
+                                <div className="col-md-4">
+                                    <h3 className="card-title">{weather.name}</h3>
+
+                                    <p className="card-date">{date}</p>
+                                    <h1 className="card-temp">{(weather.main.temp -273.15).toFixed(1)}ºC</h1>
+                                    <p className="card-desc" ><img src={iconUrl} alt="icon"/>{weather.weather[0].description}</p>
+
+                                <img src="https://images.pexels.com/photos/1334607/pexels-photo-1334607.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" className="img-fluid rounded-start" alt="ciudad"/>
+                                </div>
+                                <div className="col-md-8">
+                                    <div className="card-body text-start mt-2">
+                                        <h5 className="card-text">Temperatura máxima: {(weather.main.temp_max -273.15).toFixed(1)}ºC</h5>
+                                        <h5 className="card-text">Temperatura mínima: {(weather.main.temp_min -273.15).toFixed(1)}ºC</h5>
+                                        <h5 className="card-text">Temperatura térmica: {(weather.main.feels_like -273.15).toFixed(1)}ºC</h5>
+                                        <h5 className="card-text">Humedad: {weather.main.humidity}%</h5>
+                                        <h5 className="card-text">Velocidad del viento: {weather.wind.speed}m/s</h5>
+
+                                        
+                                    </div>
+                                    <hr/>
+                                    <div className="row mt-4">
+                                        <div className="col">
+                                            <p>{forecastDate3}h</p>
+                                            <p className="description"><img src={iconUrl3} alt="icon"/>{forecast.list[1].weather[0].description}</p>
+                                            <p className="temp">{(forecast.list[1].main.temp -273.15).toFixed(1)}ºC</p>
+                                        </div>
+
+                                        <div className="col">
+                                            <p>{forecastDate6}h</p>
+                                            <p className="description"><img src={iconUrl3} alt="icon"/>{forecast.list[1].weather[0].description}</p>
+                                            <p className="temp">{(forecast.list[2].main.temp -273.15).toFixed(1)}ºC</p>
+                                        </div>
+
+                                        <div className="col">
+                                            <p>{forecastDate9}h</p>
+                                            <p className="description"><img src={iconUrl3} alt="icon"/>{forecast.list[1].weather[0].description}</p>
+                                            <p className="temp">{(forecast.list[3].main.temp -273.15).toFixed(1)}ºC</p>
+                                        </div>
+
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+
+                ):(
+                    <h2 className="text-dark">Sin datos</h2>
+                )
+            }
+
+
+        </div>
+
+    );
+}
+
+export default Card;

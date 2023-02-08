af5619dcc7d9a5ed13e3553c4ebcdf57b07aaba2
yolags <yoleanny.garcia@gail.com>
2023-02-08 14:35:59 +0100

    carrito

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
+import { useState } from "react";
 import './App.css';
-{/* import NOMBREdelCOMPONENTE from './components/UBICACIONdelCOMPONENTE'; */}
+import './styles-carrito/carritoList.css';
+import { Header } from './components/container/carrito';
+import { ProductList } from './components/container/productList';
+/* import NOMBREdelCOMPONENTE from './components/UBICACIONdelCOMPONENTE'; */
 
 function App() {
+  const [allProducts, setAllProducts] = useState([]);
+	const [total, setTotal] = useState(0);
+	const [countProducts, setCountProducts] = useState(0);
+
   return (
     <div className="App">
+      {/* <h1>Hola chicas</h1> */}
       {/* aquí declaramos nuestro COMPONENTE propio */}
       {/* <Componente></Componente> */}
+
+      <Header
+        allProducts={allProducts}
+        setAllProducts={setAllProducts}
+        total={total}
+        setTotal={setTotal}
+        countProducts={countProducts}
+        setCountProducts={setCountProducts}
+        />
+      <ProductList
+        allProducts={allProducts}
+        setAllProducts={setAllProducts}
+        total={total}
+        setTotal={setTotal}
+        countProducts={countProducts}
+        setCountProducts={setCountProducts}
+        />
+
     </div>
   );
 }

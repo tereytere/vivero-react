18b09c1862e06757e747cdeb6130626a5b454da7
Teresa <teresa.ambroa@hotmail.com>
2023-02-10 12:05:52 +0100

    logo

--- src/App.js
+++ src/App.js
@@ -14,7 +14,11 @@ function App() {
       <div className="socorro">
         <h2 className= "about-us">
           <strong>¡Te damos la Bienvenida a Ogilvy!</strong></h2>
-          <br></br><br></br>
+          <br />
+          <img className="logo" src="./LOGO.svg" alt="Ogilvy" />
+          <br />
+          <br />
+          <br />
           <p>En nuestro vivero online el verde nos hace felices, es sinónimo de bienestar y salud, y cuando hablamos de plantas todavía más porque limpian, purifican el aire, absorben los malos olores, mejoran el estado de ánimo y ayudan a dormir mejor. ¡Ródeate de verde!</p>
       </div>
       <Viveros />
